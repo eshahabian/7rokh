79ba9e2e4342a44832e1f2fa9e4ff5770a4cc4cb
Emad <eshahabian@gmail.com>
2026-07-27 12:05:00 +0330

    menu36

--- includes/layout.php
+++ includes/layout.php
@@ -25,7 +25,7 @@ function casting_render_head(string $title, string $body_class = ''): void
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="<?= $css ?>?v=97">
+  <link rel="stylesheet" href="<?= $css ?>?v=98">
   <script>
     (function () {
       try {
