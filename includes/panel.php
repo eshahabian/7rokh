3aeac1a74d41401e7c9b9599d709adcbfe6aa321
Emad <eshahabian@gmail.com>
2026-08-14 21:17:32 +0330

    update 174

--- includes/panel.php
+++ includes/panel.php
@@ -609,9 +609,6 @@ function casting_render_panel_sidebar(string $active, string $page_title = ''): 
         $nav_ctx['highlight'] = false;
         casting_render_panel_nav_groups($nav_ctx);
         ?>
-        <div class="panel-nav-theme">
-          <?php casting_render_theme_toggle(); ?>
-        </div>
       </nav>
       <?php if ($admin_nav) : ?>
         <p class="panel-sidebar-title panel-sidebar-title-admin">مدیریت</p>
